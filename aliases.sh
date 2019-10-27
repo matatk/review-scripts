24b2f0597567af0db12b50ad9f6a50bde736747d
Matthew Tylee Atkinson <matatk@gmail.com>
2019-10-27 23:39:40 +0000

    feat(review.next, aliases): Support arbitrary commits

--- aliases.sh
+++ aliases.sh
@@ -1,4 +1,5 @@
 # shellcheck shell=sh
+# These all seem like a good idea
 alias rnt='review.next txt'
 alias rnw='review.next web'
 alias rn='review.next'
@@ -6,4 +7,17 @@ alias rp='review.park'
 alias ru='review.unpark'
 alias rrna='review.record na'
 alias rr='review.record log'
+
+# These let you view any commit as with review.next
+review_commit_txt() {
+	review.next -c "$1" txt
+}
+alias rct=review_commit_txt
+
+review_commit_web() {
+	review.next -c "$1" web
+}
+alias rcw=review_commit_web
+
+# This one uses a wildcard, so beware
 alias rb='tar zcvf "review-state.$(date +%Y-%m-%d.%H%M).tar.gz" review.state.*'
